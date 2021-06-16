6f027eadceacade665006666d4480ad962ecb827
Askold Makaruk <askoldmakaruk@gmail.com>
2021-06-16 20:20:11 +0300

    Added model and context configuration.

--- Tournament.Api/Services/Context.cs
+++ Tournament.Api/Services/Context.cs
@@ -1,6 +1,72 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Tournament.Model;
 
 namespace Tournament.Api.Services
 {
-	public class Context : DbContext { }
+	public class Context : DbContext
+	{
+		public Context()
+		{
+			Database.EnsureCreated();
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("Data Source=database.db");
+
+			//optionsBuilder.UseSqlServer("");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Model.Tournament>(entity =>
+			{
+				entity.HasKey(e => e.TournamentID);
+				entity.HasMany(e => e.Games)
+					.WithOne(e => e.Tournament)
+					.HasForeignKey(e => e.TournamentID)
+					.OnDelete(DeleteBehavior.Cascade);
+			});
+
+			modelBuilder.Entity<TournamentTeam>(entity =>
+			{
+				entity.HasKey(e => new {e.TournamentID, e.TeamID});
+				
+				entity.HasOne(e => e.Team)
+					.WithMany(e => e.TournamentTeams)
+					.HasForeignKey(e => e.TeamID)
+					.OnDelete(DeleteBehavior.Cascade);
+
+				entity.HasOne(e => e.Tournament)
+					.WithMany(e => e.TournamentTeams)
+					.HasForeignKey(e => e.TournamentID)
+					.OnDelete(DeleteBehavior.Cascade);
+			});
+
+			modelBuilder.Entity<Game>(entity =>
+			{
+				entity.HasKey(e => e.GameID);
+
+				entity.HasOne(e => e.Team1)
+					.WithMany(e => e.Games)
+					.HasForeignKey(e => e.GameID)
+					.OnDelete(DeleteBehavior.Cascade);
+				
+				entity.HasOne(e => e.Team2)
+					.WithMany(e => e.Games)
+					.HasForeignKey(e => e.GameID)
+					.OnDelete(DeleteBehavior.Cascade);
+				
+				entity.HasOne(e => e.Winner)
+					.WithMany(e => e.Games)
+					.HasForeignKey(e => e.GameID)
+					.OnDelete(DeleteBehavior.Cascade);
+			});
+			
+			modelBuilder.Entity<Team>(entity =>
+			{
+				entity.HasKey(e => e.TeamID);
+			});
+		}
+	}
 }
--- Tournament.Model/Tournament.cs
+++ Tournament.Model/Tournament.cs
@@ -1,40 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Tournament.Model
 {
-    public class Tournament
-    {
-        public int TournamentID { get; set; }
-        public string TournamentName { get; set; }
-        public int TournamentPrizePool { get; set; }
-        public string TournamentStatus { get; set; }
+	public class Tournament
+	{
+		public int TournamentID { get; set; }
+		public string TournamentName { get; set; }
+		public int TournamentPrizePool { get; set; }
+		public string TournamentStatus { get; set; }
+		public int RoundCount { get; set; }
 
+		public ICollection<Game> Games { get; set; }
+		public ICollection<TournamentTeam> TournamentTeams { get; set; }
+	}
 
+	public class TournamentTeam
+	{
+		public int TournamentID { get; set; }
+		public int TeamID { get; set; }
+		public int TeamNumber { get; set; }
+		public int Place { get; set; }
 
-        public ICollection<Game> Games { get; set; }
+		public Team Team { get; set; }
+		public Tournament Tournament { get; set; }
+	}
 
-    }
+	public class Game
+	{
+		public int GameID { get; set; }
+		public string GameName { get; set; }
+		public int Round { get; set; }
+		public int TournamentID { get; set; }
+		
+		public Tournament Tournament { get; set; }
+		public Team Team1 { get; set; }
+		public Team Team2 { get; set; }
+		public Team Winner { get; set; }
+	}
 
-    public class Game
-    {
-        public int GameID { get; set; }
-        public string GameName { get; set; }
-        public Tournament Tournament { get; set; }
+	public class Team
+	{
+		public int TeamID { get; set; }
+		public string TeamName { get; set; }
 
-
-
-        public Team Team1 { get; set; }
-        public Team Team2 { get; set; }
-        public Team Winner { get; set; }
-
-    }
-
-    public class Team
-    {
-        public int TeamID { get; set; }
-        public string TeamName { get; set; }
-        public ICollection<Game> Games { get; set; }
-
-    }
+		public ICollection<Game> Games { get; set; }
+		public ICollection<TournamentTeam> TournamentTeams { get; set; }
+	}
 }
