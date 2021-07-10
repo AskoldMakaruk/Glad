d9d68b5de87fbc9d1977196bda381da975fd6a56
Askold Makaruk <askoldmakaruk@gmail.com>
2021-07-10 15:08:05 +0300

    fixes

--- Tournament.Api/Services/Context.cs
+++ Tournament.Api/Services/Context.cs
@@ -3,62 +3,57 @@ using Tournament.Model;
 
 namespace Tournament.Api.Services
 {
-	public class Context : DbContext
-	{
-		public DbSet<Tournament.Model.Championship> Tournaments { get; set; }
+    public class Context : DbContext
+    {
+        public DbSet<Tournament.Model.Championship> Tournaments { get; set; }
 
-		public Context()
-		{
-			Database.EnsureCreated();
-		}
+        public Context(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			optionsBuilder.UseSqlite("Data Source=database.db");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Model.Championship>(entity =>
+            {
+                entity.HasKey(e => e.TournamentID);
+                entity.HasMany(e => e.Games)
+                    .WithOne(e => e.Championship)
+                    .HasForeignKey(e => e.TournamentID)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
 
-			//optionsBuilder.UseSqlServer("");
-		}
+            modelBuilder.Entity<TournamentTeam>(entity =>
+            {
+                entity.HasKey(e => new { e.TournamentID, e.TeamID });
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.Entity<Model.Championship>(entity =>
-			{
-				entity.HasKey(e => e.TournamentID);
-				entity.HasMany(e => e.Games)
-					.WithOne(e => e.Championship)
-					.HasForeignKey(e => e.TournamentID)
-					.OnDelete(DeleteBehavior.Cascade);
-			});
+                entity.HasOne(e => e.Team)
+                    .WithMany(e => e.TournamentTeams)
+                    .HasForeignKey(e => e.TeamID)
+                    .OnDelete(DeleteBehavior.Cascade);
 
-			modelBuilder.Entity<TournamentTeam>(entity =>
-			{
-				entity.HasKey(e => new {e.TournamentID, e.TeamID});
-
-				entity.HasOne(e => e.Team)
-					.WithMany(e => e.TournamentTeams)
-					.HasForeignKey(e => e.TeamID)
-					.OnDelete(DeleteBehavior.Cascade);
-
-				entity.HasOne(e => e.Championship)
-					.WithMany(e => e.TournamentTeams)
-					.HasForeignKey(e => e.TournamentID)
-					.OnDelete(DeleteBehavior.Cascade);
-			});
-
-			modelBuilder.Entity<Game>(entity =>
-			{
-				entity.HasKey(e => e.GameID);
+                entity.HasOne(e => e.Championship)
+                    .WithMany(e => e.TournamentTeams)
+                    .HasForeignKey(e => e.TournamentID)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+            
+            modelBuilder.Entity<GameTeam>(entity => { entity.HasKey(e => new { e.GameID, e.TeamID }); });
+            
+            modelBuilder.Entity<Game>(entity =>
+            {
+                entity.HasKey(e => e.GameID);
 
                 entity.HasMany(e => e.Participants)
                     .WithOne(e => e.Game)
                     .HasForeignKey(e => e.GameID)
                     .OnDelete(DeleteBehavior.Cascade);
 
-				entity.HasOne(e => e.Winner)
-					.WithMany(e => e.WonGames)
-					.HasForeignKey(e => e.GameID)
-					.OnDelete(DeleteBehavior.Cascade);
-			});
+                entity.HasOne(e => e.Winner)
+                    .WithMany(e => e.WonGames)
+                    .HasForeignKey(e => e.GameID)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
 
             modelBuilder.Entity<Team>(entity =>
             {
